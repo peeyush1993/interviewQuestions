4548a2fe3b667de51f5b5e93b755eb6e35745e4d
Peeyush anand <peeyushknp@gmail.com>
2020-08-31 10:53:29 +0530

    initial project upload

---
+++ InterviewQuestion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace InterviewQuestion
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            B dd = new B();
+            dd.Add(1, 1);
+
+            Console.WriteLine(dd.Add(1, 1));
+            Console.ReadKey();
+        }
+    }
+
+    public class A
+    {
+      public  int Add(int i, int j)
+        {
+
+            return i + j;
+        }
+        //string Add(int i, int j) {
+        //    return "";
+        //}
+    }
+    public class B:A {
+
+     public string Add(int i, int j)
+        {
+            return "hello"+(i+j);
+        }
+    }
+
+}
