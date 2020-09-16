5f9147d239e81e9ce71c296da9512d6bcb451faa
Peeyusg Anand <peeyushknp@gmail.com>
2020-09-16 23:45:27 +0530

    Core MVC application added and console application rename

---
+++ InterviewQuestion/LinqToSql.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewQuestion
+{
+    /// <summary>
+    /// Database connectivity
+    /// SQL server
+    /// ORM -Object relational mapping 
+    /// </summary>
+  public  class LinqToSql
+    {
+    }
+}
